74574f6ba66d67dcdbfc74e55d64db322e40b72a
YANISA PONGMETHA <63010249@KMITL.AC.TH>
2020-10-26 07:33:23 +0700

    Upgrade : flip tringlepattern

--- Week4/trianglepattern.cpp
+++ Week4/trianglepattern.cpp
@@ -1,16 +1,32 @@
 #include<stdio.h>
 
-int main() {
-	int level,row,star ;
-	printf("Enter number : ");
-	scanf_s("%d", &level);
+void triPattern(int);
 
+int main()
+{
+	int level;
+	printf("Enter a positive integer : ");
+	scanf("%d", &level);
+	if (level > 0)
+	{
+		triPattern(level);
+	}
+	else
+	{
+		printf("ERROR!!");
+	}
+	return 0;
+}
+void triPattern(int level)
+{
+	int row, star;
 	for (row = 0; row < level; row++) {
-		for (star = 0; star <= row; star++) {
+		for (star = 0; star < row; star++) {
+			printf("  ");
+		}
+		for (star = row; star < level; star++) {
 			printf("* ");
 		}
 		printf("\n");
 	}
-
-	return 0;
 }
